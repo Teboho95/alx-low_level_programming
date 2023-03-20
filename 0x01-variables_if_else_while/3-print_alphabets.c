95f2267892b6ce44569e3847c806ec488c1a8bf3
Teboho95 <Teboho95>
2023-03-20 15:04:34 -0700

    lower and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main-Prints alphabets in lower case then in upper case
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = "a" ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = "A" ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
