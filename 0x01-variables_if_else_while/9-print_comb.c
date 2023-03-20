82378774319968ce3dcd1864cd59b5088c188a9e
Teboho95 <Teboho95>
2023-03-20 16:56:48 -0700

    patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main-possible combinations of single digit numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; i < 58; i++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
